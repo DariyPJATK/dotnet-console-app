609ae2a2d7caee0586d6f9013e6ef95c07903038
DariyPJATK <s32558@pjwstk.edu.pl>
2026-03-10 11:34:22 +0100

    Add basic error handling for invalid input

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
-Console.WriteLine("Enter first number:");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter second number:");
-int b = int.Parse(Console.ReadLine());
-Console.WriteLine($"Numbers entered: {a}, {b}");
 
-int sum = StatisticsHelper.Sum(a, b);
-Console.WriteLine($"Sum: {sum}");
+try
+{
+    Console.WriteLine("Enter first number:");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter second number:");
+    int b = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Numbers entered: {a}, {b}");
+
+    int sum = StatisticsHelper.Sum(a, b);
+    Console.WriteLine($"Sum: {sum}");
+}
+catch
+{
+    Console.WriteLine("Something went wrong. Input numbers only");
+}
