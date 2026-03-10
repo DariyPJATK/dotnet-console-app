bb1d1e60b874736e353337263951bb219594e555
DariyPJATK <s32558@pjwstk.edu.pl>
2026-03-10 11:29:45 +0100

    Use StatisticsHelper to calculate sum

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,3 +4,6 @@ int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter second number:");
 int b = int.Parse(Console.ReadLine());
 Console.WriteLine($"Numbers entered: {a}, {b}");
+
+int sum = StatisticsHelper.Sum(a, b);
+Console.WriteLine($"Sum: {sum}");
