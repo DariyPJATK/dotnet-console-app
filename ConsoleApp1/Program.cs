23d93aaefdb17503043216f9fc2ed10a415dfdc0
s32558 <s32558@pjwstk.edu.pl>
2026-03-16 18:35:59 +0100

    chore: update output message format on main

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ try
     int a = int.Parse(Console.ReadLine());
     Console.WriteLine("Enter second number (integer):");
     int b = int.Parse(Console.ReadLine());
-    Console.WriteLine($"Numbers entered: {a}, {b}");
+    Console.WriteLine($"Input values: {a} | {b}");
 
     int[] numbers = { a, b };
     double avg = StatisticsHelper.CalculateAverage(numbers);
